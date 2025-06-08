dcf9f4cd430297319fde3c053e27d888acb22905
David Osama <davidosamamilad@gmail.com>
2025-06-08 17:07:23 +0300

    task almost completed

--- src/app/core/services/categories/categories.service.ts
+++ src/app/core/services/categories/categories.service.ts
@@ -9,7 +9,18 @@ export class CategoriesService {
 
   constructor(private httpClient:HttpClient) { }
 
+myToken = localStorage.getItem('token')
+
  getAllGategories():Observable<any>{
   return this.httpClient.get('https://flower.elevateegy.com/api/v1/categories')
  } 
+
+
+ removeCategory(id:string):Observable<any>{
+return this.httpClient.delete(`https://flower.elevateegy.com/api/v1/categories/${id}`,{
+  headers:{
+    token: this.myToken  ?? ''
+  }
+})
+ }
 }
--- src/app/pages/add-categories/add-categories.component.ts
+++ src/app/pages/add-categories/add-categories.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+import { ButtonModule } from 'primeng/button';
 
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-add-categories',
-  imports: [],
+  imports: [MessageModule,InputTextModule,ButtonModule,ReactiveFormsModule],
   templateUrl: './add-categories.component.html',
   styleUrl: './add-categories.component.scss'
 })
 export class AddCategoriesComponent {
+addCatgory = new FormGroup({
+  name : new FormControl(null),
+  image: new FormControl(null)
+})
 
+sendData(){
+  console.log(this.addCatgory)}
 }
